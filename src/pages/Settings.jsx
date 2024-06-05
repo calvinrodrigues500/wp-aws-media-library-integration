35c8094005b6c6eb6a9f5830fe74dabda6d2f8f3
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-06-05 23:51:25 +0530

    Add config, refactor settings page into components

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n'
-import { Panel, PanelBody, PanelRow, Button, Flex, FlexItem, TextControl, Dropdown, MenuGroup, MenuItem } from '@wordpress/components';
-import {
-    __experimentalText as Text,
-    TextInput,
-    __experimentalVStack as VStack
-} from '@wordpress/components';
+import { Flex } from '@wordpress/components';
+
 import { useState } from '@wordpress/element';
-
-
-// import { awsAuthenticationMethods } from '../utils/awsAuthenticationMethods';
-// import { Dropdown, FormInput } from '../components';
+import { ConfigurationSetting } from '../components/settings';
 // 
 const Settings = () => {
     // const [authMethod, setAuthMethod] = useState(awsAuthenticationMethods[0].id);
@@ -28,84 +21,15 @@ const Settings = () => {
 
     const { setSettings } = useDispatch('my-shop');
 
-    console.log('--------- ', settings);
-
-    const dorpVal = ['hi', 'hel', 'dd'];
-
-
     return (
-        <Panel
-            header={__('WP AWS Media Library Integration')}
-        >
-            <PanelBody
-                title={__('AWS Configuration', 'wp-aws-mli')}
-                initialOpen={true}
-            >
-                <PanelRow>
-                    <Flex
-                        justify='space-between'
-                    >
-                        <FlexItem>
-                            <Text
-                                size='15'
-                                fontWeight='bold'
-                            >
-                                {__('Select an authentication method', 'wp-aws-mli')}
-                            </Text>
-                            <Dropdown
-                                popoverProps={ { placement: 'bottom-start' } }
-                                renderToggle={ ( { isOpen, onToggle, onClose } ) => (
-                                <Button
-                                    variant="primary"
-                                    onClick={ onToggle }
-                                    aria-expanded={ isOpen }
-                                >
-                                    Toggle Dropdown!
-                                </Button>
-                                ) }
-                                renderContent={ () => (
-                                    <MenuGroup
-                                        onClick={onclose}
-                                    >
-                                    <MenuItem>
-                                        Menu Item 1
-                                    </MenuItem>
-                                    <MenuItem>
-                                        Menu Item 2
-                                    </MenuItem>
-                                    </MenuGroup>
-                                ) }
-                            />
-                        </FlexItem>
 
-                        <VStack>
-                            <TextControl
-                                type='password'
-                                value={settings.access_key_id}
-                                label={__('Access Key ID', 'wp-aws-mli')}
-                                onChange={ (value) => {
-                                    setSettings({...settings, access_key_id: value})
-                                }}
-                            />
-                            <TextControl
-                                type='password'
-                                value={settings.secret_access_key}
-                                label={__('Secret Access Key', 'wp-aws-mli')}
-                                onChange={ (value) => 
-                                    setSettings({...settings, secret_access_key: value})
-                                }
-                            />
-                        </VStack>
-                    </Flex>
-                </PanelRow>
-                <Button
-                    variant='primary'
-                    onClick={handleSave}
-                >
-                    {__('Save', 'wp-aws-mli')}
-                </Button>
-            </PanelBody>
-        </Panel>
+        <Flex
+            direction='column'
+            justify='center'
+            align='center'
+        >
+            <ConfigurationSetting />
+        </Flex>
     )
 }
 
